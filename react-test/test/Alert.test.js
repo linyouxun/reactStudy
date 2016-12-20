e172b37db44423d57e4d155bf311455e18e6c167
youxunlin <youxun.lin@360gst.com>
2016-12-20 18:27:32 +0800

    react test

--- react-test/test/Alert.test.js
+++ react-test/test/Alert.test.js
@@ -1,34 +1,19 @@
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
-import { expect } from 'chai';
+// import { expect } from 'chai';
+import {describeWithDOM,mount,shallow,spyLifecyle} from "enzyme";
 import Alert from '../src/Alert';
 
-function shallowRender(Component) {
-  const renderer = TestUtils.createRenderer();
-  renderer.render(<Component/>);
-  return renderer.getRenderOutput();
-}
-
-describe('Shallow Rendering', function () {
-  it('Alert', function () {
-    const app = shallowRender(Alert);
-    console.log(app);
-    console.log(app.props.children);
-    // component's shallow rendering has props.children
-    expect(app.props.children[0].type).to.equal('div');
-    // expect(app.props.children[0].props.children).to.equal('Todos');
-  });
-});
-
-describe('DOM Rendering', function () {
-  it('Alert DOM', function () {
-    const app = TestUtils.renderIntoDocument(<Alert/>);
-    console.log(app);
-    // let todoItems = TestUtils.scryRenderedDOMComponentsWithTag(app, 'li');
-    // let todoLength = todoItems.length;
-    // let deleteButton = todoItems[0].querySelector('button');
-    // TestUtils.Simulate.click(deleteButton);
-    // let todoItemsAfterClick = TestUtils.scryRenderedDOMComponentsWithTag(app, 'li');
-    // expect(todoItemsAfterClick.length).to.equal(todoLength - 1);
-  });
+describe('Shallow Rendering',()=>{
+  describeWithDOM("render DON lifecycle",()=>{
+    it('Alert', function () {
+      // spyLifecyle(Alert);
+      const app = mount(<Alert/>);
+      console.log(Alert.prototype.componentDidMount.calledOnce);
+    });
+  })
+  // it("Alert find div2",()=>{
+  //   const app = shallow(<Alert/>);
+  //   expect(app.find(".div1")).to.have.length(1);
+  // })
 });
