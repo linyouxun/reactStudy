40920cd6260dc589e7c790d12dfde6da9ae54c79
youxunlin <youxun.lin@360gst.com>
2016-12-22 11:54:27 +0800

    将webpack 里面的静态库分离抽取出来

--- react-redux-study/webpack.config.js
+++ react-redux-study/webpack.config.js
@@ -1,6 +1,19 @@
 // var path = require('path'),
 //     ExtractTextPlugin = require('extract-text-webpack-plugin'),
 //     autoprefixer = require('autoprefixer');
+var path = require('path');
+var webpack = require('webpack');
+var node_modules = path.resolve(__dirname, 'node_modules');
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
+var pathToReactDom = path.resolve(node_modules, 'react-dom/dist/react-dom.min.js');
+var pathToReactRedux = path.resolve(node_modules, 'react-redux/dist/react-redux.min.js');
+
+var pathToReduxThunk = path.resolve(node_modules, 'redux-thunk/dist/redux-thunk.min.js');
+var pathToReduxLogger = path.resolve(node_modules, 'redux-logger/dist/index.min.js');
+
+
+var pathToRedux = path.resolve(node_modules, 'redux/dist/redux.min.js');
+
 
 module.exports = {
   entry: './index.jsx',
@@ -9,10 +22,23 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/',
   },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM",
+  },
   resolve:{
     extensions:["",".js",".jsx","css","less","scss","png","jpg"],
+    alias: {
+      'react$': pathToReact,
+      'react-dom$':pathToReactDom,
+      'react-redux$':pathToReactRedux,
+      'redux$':pathToRedux,
+      'redux-chunk$':pathToReduxThunk,
+      'redux-logger$':pathToReduxLogger,
+    },
   },
   module: {
+    // noParse: [/react/],
     loaders: [
       {
         test: /\.jsx?$/, exclude: /node_modules/,
@@ -26,6 +52,15 @@ module.exports = {
       // },
     ]
   },
+  plugins: [
+    // new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ],
   // postcss: function () {
   //   return [autoprefixer];
   // },
